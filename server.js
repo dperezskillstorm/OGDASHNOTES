927a666f775d6d0585535c2daedb6b3396a8bff3
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2022-09-24 11:31:18 -0500

    update Cors to match client

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 let corsOptions = {
-    origin: "http://localhost:3000"
+    origin: "https://client-8hbf.vercel.app/"
 };
 
 const PORT = process.env.PORT ||8080;
@@ -23,13 +23,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 //Different Cors Method
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 
   //create a logger
   const logger = winston.createLogger({
